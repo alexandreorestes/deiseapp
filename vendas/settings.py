557933225240087bab6a250c33f2535287607eea
alexandreorestes <alexandre.ufu@gmail.com>
2024-01-04 17:18:39 -0300

    pasta diferente para css

--- vendas/settings.py
+++ vendas/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'css')
+STATIC_ROOT = os.path.join(BASE_DIR, 'appvendas/css')
 
 
 
